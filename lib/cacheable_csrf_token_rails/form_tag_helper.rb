6d6508ac4d37d72427d3bdb387918f55e5b7eefe
Sijawusz Pur Rahnama <sija@sija.pl>
2014-04-28 01:18:24 +0200

    Changeing namespace of token_tag method, Rails 4+ only

--- lib/cacheable_csrf_token_rails/form_tag_helper.rb
+++ lib/cacheable_csrf_token_rails/form_tag_helper.rb
@@ -1,6 +1,6 @@
 require 'action_view/helpers/form_tag_helper'
 
-module ActionView::Helpers::FormTagHelper
+module ActionView::Helpers::UrlHelper
   def token_tag(token=nil)
     if token != false && protect_against_forgery?
       tag(:input, :type => "hidden", :name => request_forgery_protection_token.to_s, :value => CacheableCsrfTokenRails::TokenPlaceholder)
